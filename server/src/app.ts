e5c74f784ab530597bcdfd7acea783b67122df92
Arghya <arghyadas242004@gmail.com>
2024-08-23 19:53:10 +0530

    try: server err fix

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,11 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const CLIENT = process.env.CLIENT || "http://localhost:3000";
 
-app.use(
-  cors({
-    origin: [CLIENT],
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
